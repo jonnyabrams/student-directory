9c28328a4912e3c34b7c05562bfff4f9c7cbbb96
Jonny Abrams <jonnyabrams@hotmail.com>
2022-02-18 14:29:56 +0000

    Debugging exercise

--- directory_2.rb
+++ directory_2.rb
@@ -1,11 +1,15 @@
 def input_students
+    puts "Please enter the names of the students"
+    puts "To finish, just hit return twice"
     puts "Please enter the name of a student"
     puts "To finish, leave one or both fields blank"
     students = []
     name = gets.chomp
     puts "Please enter a hobby"
     hobby = gets.chomp
-  
+
+    while !name.empty? do
+      students << {name: name, cohort: :november}
     while !name.empty? && !hobby.empty? do
       students << {name: name, hobby: hobby, cohort: :november}
       puts "Now we have #{students.count} students"
@@ -16,23 +20,11 @@ def input_students
     end
     students
   end
-  
-  def print_header
-    puts "The students of Villains Academy"
-    puts "-------------"
-  end
-  
+@@ -19,7 +24,7 @@ def print_header
+
   def print(students)
     students.each_with_index do |student, index|
+      puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name].length < 12
       puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort), who likes #{student[:hobby].downcase}" if student[:name].length < 12
     end 
   end 
-  
-  def print_footer(students)
-    puts "Overall, we have #{students.count} great students"
-  end 
-  
-  students = input_students
-  print_header
-  print(students)
-  print_footer(students)
